dca7750e7267e20b0d443f2473ff214e5c5de571
Rajashekar582 <rajashekarg195a2gmail.com>
2024-07-09 18:23:36 +0530

    new features

--- bank_approve.by.me/src/bank_approve/officer_approvel.java
+++ bank_approve.by.me/src/bank_approve/officer_approvel.java
@@ -1,16 +1,18 @@
 package bank_approve;
 
 public class officer_approvel {
-	public static void officer(){
+	public static void officer()
 	   // if(mainf.deputyapprove()==true)
 		mainf f=new mainf();
 	     if(f.deputyapprove()==true)
-	    {
+	    
 		System.out.println(mainf.k+" is authorised by officer");
 		System.out.println("okay");
 		
+		System.out.println("what new changes");
+		
         
-		}else 
+		else 
 		{System.out.println("not approved");}
-	}
+	
 }
